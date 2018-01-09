dbc247b8d21facf25f5fda7c6d289011d7a95e88
Znagger <goodnews2day@gmail.com>
2018-01-09 17:36:12 +0100

    First upload

---
+++ Pins.h
@@ -0,0 +1,14 @@
+#ifndef PINS_H
+#define PINS_H
+
+/*** ATtiny85 ******/
+#define DIO0 PB3     
+#define NSS_RFM PB4       
+#define NSS_BME280 PB5
+
+/*** ATmega328p ******/
+//#define DIO0 8     
+//#define NSS_RFM 9     
+//#define NSS_BME280 10
+
+#endif
