5656cac715148a3f6c192e9b6b91b64ac74d7e6d
brentru <brent@adafruit.com>
2018-10-16 09:34:14 -0400

    Updates to match proposed changes in the .c file.  #defines to manages frequency map selection and data rate selection.  Addition of public class variable txrandomNum.

--- ATtinyLoRa.h
+++ ATtinyLoRa.h
@@ -6,29 +6,55 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
+/*
+*****************************************************************************************
+* Description: TinyLora Configuration Options
+*****************************************************************************************
+*/
+ /* Set TTN frequecy plan EU863, AU915, AS920, US902 */
+ //#define EU863
+//#define US902
+#define AU915
+//#define AS920
+ /* Define fixed datarate */
+ //#define SF12BW125
+//#define SF11BW125
+//#define SF10BW125
+//#define SF9BW125
+//#define SF8BW125
+#define SF7BW125
+//#define SF7BW250
+
+ /*
+*****************************************************************************************
+* Description: TinyLora Class
+*****************************************************************************************
+*/
 class TinyLoRa
 {
 	public:
-	uint16_t frameCounter;
-	void begin(void);
-	void sendData(unsigned char *Data, unsigned char Data_Length, unsigned int Frame_Counter_Tx);
+		uint8_t txrandomNum;
+		uint16_t frameCounter;
+		void begin(void);
+		void sendData(unsigned char *Data, unsigned char Data_Length, unsigned int Frame_Counter_Tx);
 
 	private:
-	uint8_t randomNum;
-	static const unsigned char S_Table[16][16];
-	void RFM_Send_Package(unsigned char *RFM_Tx_Package, unsigned char Package_Length);
-	void RFM_Write(unsigned char RFM_Address, unsigned char RFM_Data);	
-	void Encrypt_Payload(unsigned char *Data, unsigned char Data_Length, unsigned int Frame_Counter, unsigned char Direction);
-	void Calculate_MIC(unsigned char *Data, unsigned char *Final_MIC, unsigned char Data_Length, unsigned int Frame_Counter, unsigned char Direction);
-	void Generate_Keys(unsigned char *K1, unsigned char *K2);
-	void Shift_Left(unsigned char *Data);
-	void XOR(unsigned char *New_Data,unsigned char *Old_Data);
-	void AES_Encrypt(unsigned char *Data, unsigned char *Key);
-	void AES_Add_Round_Key(unsigned char *Round_Key, unsigned char (*State)[4]);
-	unsigned char AES_Sub_Byte(unsigned char Byte);
-	void AES_Shift_Rows(unsigned char (*State)[4]);
-	void AES_Mix_Collums(unsigned char (*State)[4]);
-	void AES_Calculate_Round_Key(unsigned char Round, unsigned char *Round_Key);
+		uint8_t randomNum;
+		static const unsigned char LoRa_Frequency[8][3];
+		static const unsigned char S_Table[16][16];
+		void RFM_Send_Package(unsigned char *RFM_Tx_Package, unsigned char Package_Length);
+		void RFM_Write(unsigned char RFM_Address, unsigned char RFM_Data);
+		void Encrypt_Payload(unsigned char *Data, unsigned char Data_Length, unsigned int Frame_Counter, unsigned char Direction);
+		void Calculate_MIC(unsigned char *Data, unsigned char *Final_MIC, unsigned char Data_Length, unsigned int Frame_Counter, unsigned char Direction);
+		void Generate_Keys(unsigned char *K1, unsigned char *K2);
+		void Shift_Left(unsigned char *Data);
+		void XOR(unsigned char *New_Data, unsigned char *Old_Data);
+		void AES_Encrypt(unsigned char *Data, unsigned char *Key);
+		void AES_Add_Round_Key(unsigned char *Round_Key, unsigned char(*State)[4]);
+		unsigned char AES_Sub_Byte(unsigned char Byte);
+		void AES_Shift_Rows(unsigned char(*State)[4]);
+		void AES_Mix_Collums(unsigned char(*State)[4]);
+		void AES_Calculate_Round_Key(unsigned char Round, unsigned char *Round_Key);
 
 };
 
